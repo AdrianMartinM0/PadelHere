7a6d4cc425b10e5d9786dc949d0dd863fb6a6376
Adrián Martín Muñoz <adrianmartinm04@gmail.com>
2025-04-10 18:17:34 +0200

    ajuste de las rutas de imagenes para que se reconozca en el despliegue

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -5,7 +5,7 @@ import HeaderSinLog from "./HeaderSinLog"
 const Header = () => {
   return (
     <header className="flex justify-between px-24 shadow-xl bg-white">
-        <img className="h-24" src="./public/images/PadelHere_logo.png" alt="Logo de PadelHere" />
+        <img className="h-24" src="./images/PadelHere_logo.png" alt="Logo de PadelHere" />
         <HeaderSinLog/>
     </header>
   )
--- front/src/components/InicioJugador.tsx
+++ front/src/components/InicioJugador.tsx
@@ -3,7 +3,7 @@
 const InicioJugador = () => {
   return (
     <div className="bg-white h-full w-full rounded-4xl flex justify-center items-center overflow-hidden">
-        <img className="object-cover transform scale-x-[-1]" src="./public/images/eresJugador.jpg" alt="Imagen Jugador de Padel" />
+        <img className="object-cover transform scale-x-[-1]" src=".images/eresJugador.jpg" alt="Imagen Jugador de Padel" />
     </div>
   )
 }
