4b0dfcc35699e108a7b88e7877b3c8b63762361d
Akuwu <151742060+AdrianMartinM0@users.noreply.github.com>
2025-06-16 15:42:29 +0200

    Update InicioNoticias.tsx

--- front/src/components/Inicio/InicioNoticias.tsx
+++ front/src/components/Inicio/InicioNoticias.tsx
@@ -8,13 +8,13 @@ const InicioNoticias = () => {
       />
       <div className="bg-black/60 w-full h-full rounded-3xl flex flex-col justify-center items-center absolute top-0 left-0 px-2 sm:px-4 text-center">
         <p className="text-white text-base sm:text-2xl lg:text-4xl font-semibold drop-shadow">
-          Actualmente estamos en desarrollo.
+          Actualmente seguimos en desarrollo.
         </p>
         <p className="text-white text-xs sm:text-xl lg:text-2xl mt-2">
-          Próximamente en funcionamiento.
+          Funcionamineto bajo cambios y mejoras.
         </p>
         <p className="text-white text-xs sm:text-lg lg:text-xl mt-2">
-          Fecha estimada <span className="font-bold">15/06/2025</span>.
+          Siguiente mejora relacionanda con <span className="font-bold">el perfil</span>.
         </p>
       </div>
     </>
